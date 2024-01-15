b8e948c987de7949c423fc280f241d09044c1869
Nicholas Manning <njmanning212@gmail.com>
2024-01-15 14:20:25 -0600

    add signInRequest dao

---
+++ src/main/java/com/njman/ptrnapi/daos/requests/SignInRequest.java
@@ -0,0 +1,15 @@
+package com.njman.ptrnapi.daos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInRequest {
+    private String email;
+    private String password;
+}
