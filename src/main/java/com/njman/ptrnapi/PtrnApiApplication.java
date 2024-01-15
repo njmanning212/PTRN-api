391146fd1c0717d036df6154bc4563873380757d
Nicholas Manning <njmanning212@gmail.com>
2024-01-15 11:56:13 -0600

    initial commit

---
+++ src/main/java/com/njman/ptrnapi/PtrnApiApplication.java
@@ -0,0 +1,13 @@
+package com.njman.ptrnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtrnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtrnApiApplication.class, args);
+	}
+
+}
