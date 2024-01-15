a09aa9e622ead73509edf2c5246eb52716e8aadb
Nicholas Manning <njmanning212@gmail.com>
2024-01-15 12:11:37 -0600

    add profile and user model and role enum

---
+++ src/main/java/models/Profile.java
@@ -0,0 +1,30 @@
+package models;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "_profile")
+public class Profile {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    User user;
+
+}
+
---
+++ src/main/java/models/Role.java
@@ -0,0 +1,19 @@
+package models;
+
+public enum Role {
+    ADMIN(500),
+    CLINIC_ADMIN(400),
+    THERAPIST(300),
+    RECEPTIONIST(200),
+    PATIENT(100);
+
+    private final int value;
+
+    private Role(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
