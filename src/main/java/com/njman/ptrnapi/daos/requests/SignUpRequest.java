826a05527dba784843d8b2ded6cc2f5d8ab803d7
Nicholas Manning <njmanning212@gmail.com>
2024-01-15 12:28:30 -0600

    add dao file structure, add signUpRequest dao

---
+++ src/main/java/com/njman/ptrnapi/daos/requests/SignUpRequest.java
@@ -0,0 +1,18 @@
+package com.njman.ptrnapi.daos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+}
