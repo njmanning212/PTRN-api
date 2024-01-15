4a9eab0f6d805149c5ad8c4ab34a5c69c84b2c55
Nicholas Manning <njmanning212@gmail.com>
2024-01-15 14:21:23 -0600

    add JwtAuthenticationResponse dao

---
+++ src/main/java/com/njman/ptrnapi/daos/responses/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.njman.ptrnapi.daos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    public String token;
+}
